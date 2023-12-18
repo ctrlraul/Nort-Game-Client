c76f26d343e325a3114c6b2b4b9386720c044beb
Raul <mailctrlraul@gmail.com>
2023-12-18 00:03:48 -0300

    Added radar skill, which should enable minimap

---
+++ src/Nort/Skills/RadarSkill.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+namespace Nort.Skills;
+
+public partial class RadarSkill : SkillNode
+{
+    public override void Fire()
+    {
+        throw new Exception("Nah");
+    }
+}
